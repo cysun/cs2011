732ad122696cc931c53322b52c2066c09364039e
Chengyu Sun <csun@calstatela.edu>
2018-08-12 17:03:12 -0700

    Added Chapter 1 Examples

---
+++ src/main/java/chapter01/ShowSyntaxErrors.java
@@ -0,0 +1,9 @@
+package chapter01;
+
+public class ShowSyntaxErrors {
+    // public static main(String[] args) {
+    public static void main(String[] args) {
+        // System.out.println("Welcome to Java);
+        System.out.println("Welcome to Java");
+    }
+}
